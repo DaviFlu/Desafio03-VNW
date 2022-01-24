8e55d16318e5cfbc71ac066a32398a505e4cf8fd
unknown <davisoares1999@hotmail.com>
2022-01-24 18:37:13 -0300

    upando o desafio03 do vnw

---
+++ script.js
@@ -0,0 +1,33 @@
+/* QUESTÃO 1 */
+/* FOR */
+for(let i = 1; i<=10; i++){
+    console.log(i)
+}
+
+/* QUESTÃO 2 */
+/* WHILE */
+let i = 10
+
+while (i >= 1 ){
+    console.log(i)
+    i--
+}
+
+console.log("ESPAÇO")
+
+/* QUESTÃO 3 */
+/* FOR */
+for(let y = 1; y<=100; y+=2){
+    console.log(y)
+}
+
+/* QUESTÃO 4 */
+/* WHILE */
+let y = 0
+
+while( y <= 100){
+    if (y % 2 == 0){
+        console.log(y)
+    }
+    y++
+}
